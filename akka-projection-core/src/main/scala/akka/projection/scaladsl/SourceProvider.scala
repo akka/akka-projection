1ef8ada8af514a5ed45296b451418c4a6009ac00
Renato Cavalcanti <renato@cavalcanti.be>
2019-03-30 01:57:41 +0100

    Added example in which the Envelope is exposed to the ProjectionHandler

--- akka-projection-core/src/main/scala/akka/projection/scaladsl/SourceProvider.scala
+++ akka-projection-core/src/main/scala/akka/projection/scaladsl/SourceProvider.scala
@@ -8,7 +8,6 @@ import akka.stream.scaladsl.Source
 
 trait SourceProvider[Envelope, Payload, Offset] {
 
-
   /**
    * Provides a Source[S, _] starting from the passed offset.
    * When Offset is None, the Source will start from the first element.
@@ -19,6 +18,24 @@ trait SourceProvider[Envelope, Payload, Offset] {
   def extractOffset(envelope: Envelope): Offset
 
   def extractPayload(envelope: Envelope): Payload
-  
+
+
 }
 
+object SourceProvider {
+
+  /**
+   * This method converts a [[SourceProvider]] into one that will not extract the [[Payload]] and instead
+   * will deliver the [[Envelope]] to the [[ProjectionHandler]].
+   */
+  def exposeEnvelope[Envelope, Payload, Offset](sourceProvider: SourceProvider[Envelope, Payload, Offset]) = {
+    new SourceProvider[Envelope, Envelope, Offset] {
+
+      override def source(offset: Option[Offset]): Source[Envelope, _] = sourceProvider.source(offset)
+
+      override def extractOffset(envelope: Envelope): Offset = sourceProvider.extractOffset(envelope)
+
+      override def extractPayload(envelope: Envelope): Envelope = envelope
+    }
+  }
+}
