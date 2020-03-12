bb230e6eef75688ae3f85f3c60fead7bacf3343c
Renato Cavalcanti <renato@cavalcanti.be>
2020-03-12 13:37:30 +0100

    extra run process to method

--- akka-projection-core/src/main/scala/akka/projection/scaladsl/Projection.scala
+++ akka-projection-core/src/main/scala/akka/projection/scaladsl/Projection.scala
@@ -6,14 +6,13 @@ package akka.projection.scaladsl
 
 import akka.stream.{KillSwitch, KillSwitches, Materializer}
 import akka.stream.scaladsl.{Keep, Sink, Source}
-
+import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 
 case class Projection[Envelope, Event, Offset, Result](sourceProvider: SourceProvider[Offset, Envelope],
                                                        envelopeExtractor: EnvelopeExtractor[Envelope, Event, Offset],
                                                        runner: ProjectionRunner[Offset, Result],
                                                        handler: EventHandler[Event, Result]) {
-
 
   private var shutdown: Option[KillSwitch] = None
 
@@ -25,26 +24,24 @@ case class Projection[Envelope, Event, Offset, Result](sourceProvider: SourcePro
       Source
         .fromFuture(offsetFut.map(sourceProvider.source))
         .flatMapConcat(identity)
+        .mapAsync(1)(processEnvelope)
 
-    val src =
-      // TODO: runner could return a Flow that defines the mapAsync 
-      // so different implementations could decide if it makes sense or not
-      source.mapAsync(1) { envelope =>
-        // the runner is responsible for the call to EventHandler
-        // so it can define what to do with the Offset: at-least-once, at-most-once, effectively-once
-        runner.run(envelopeExtractor.extractOffset(envelope))  { () =>
-          handler.onEvent(envelopeExtractor.extractPayload(envelope))
-        }
-      }
-
-
-    val (killSwitch, streamDone) = src
+    val (killSwitch, streamDone) = source
       .viaMat(KillSwitches.single)(Keep.right)
       .toMat(Sink.ignore)(Keep.both)
       .run()
 
     shutdown = Some(killSwitch)
 
+  }
+
+  private def processEnvelope(envelope: Envelope)(implicit ex: ExecutionContext): Future[Offset] =  {
+    // the runner is responsible for the call to EventHandler
+    // so it can define what to do with the Offset: at-least-once, at-most-once, effectively-once
+    val offset = envelopeExtractor.extractOffset(envelope)
+    runner.run(offset) { () =>
+      handler.onEvent(envelopeExtractor.extractPayload(envelope))
+    }.map(_ => offset)
   }
 
   def stop(): Unit = shutdown.foreach( _.shutdown() )
