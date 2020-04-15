0fce404493f4d2176b31203650c1205761f7b733
Renato Cavalcanti <renato@cavalcanti.be>
2020-04-15 11:44:48 +0200

    extends test to verify retries within max timeout

--- akka-projection-testkit/src/test/scala/akka/projection/testkit/ProjectionTestKitSpec.scala
+++ akka-projection-testkit/src/test/scala/akka/projection/testkit/ProjectionTestKitSpec.scala
@@ -4,18 +4,16 @@
 
 package akka.projection.testkit
 
-import akka.{ Done, NotUsed }
 import akka.actor.ClassicActorSystemProvider
-import akka.actor.testkit.typed.scaladsl.{ ScalaTestWithActorTestKit, TestProbe }
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import akka.projection.Projection
-import akka.stream.{ KillSwitch, KillSwitches }
-import akka.stream.scaladsl.Source
-import akka.stream.scaladsl.{ Keep, Sink }
-import akka.stream.testkit.TestSubscriber
-import akka.stream.testkit.scaladsl.TestSink
+import akka.stream.scaladsl.{ DelayStrategy, Keep, Sink, Source }
+import akka.stream.{ DelayOverflowStrategy, KillSwitch, KillSwitches }
+import akka.{ Done, NotUsed }
+import org.scalatest.exceptions.TestFailedException
 import org.scalatest.wordspec.AnyWordSpecLike
-import akka.actor.typed.scaladsl.adapter._
 
+import scala.concurrent.duration._
 import scala.concurrent.{ Future, Promise }
 
 class ProjectionTestKitSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
@@ -24,23 +22,59 @@ class ProjectionTestKitSpec extends ScalaTestWithActorTestKit with AnyWordSpecLi
 
   "ProjectionTestKit" must {
 
-    "run a projection" in {
-      val probe = testKit.createTestProbe[Int]("projection-probe")
-      val prj = TestProjection(Source(1 to 5), probe)
+    "assert progress of a projection" in {
 
-      val stopped =
-        projectionTestKit.run(prj) {
-          probe.expectMessage(1)
-          probe.expectMessage(2)
-          probe.expectMessage(3)
-          probe.expectMessage(4)
-          probe.expectMessage(5)
-        }
+      val concatenatedRef = new StringBuffer("")
+      val src =
+        Source(1 to 20)
+      val prj = TestProjection(src, concatenatedRef, _ <= 6)
+
+      // stop as soon we observe that all expected elements passed through
+      projectionTestKit.run(prj) {
+        concatenatedRef.toString shouldBe "1-2-3-4-5-6"
+      }
     }
 
+    "retry assertion function until it succeeds within a max timeout" in {
+
+      val concatenatedRef = new StringBuffer("")
+
+      // simulate slow stream by adding some delay on each element
+      val src =
+        Source(1 to 20).delayWith(
+          () => DelayStrategy.linearIncreasingDelay(200.millis, _ => true),
+          DelayOverflowStrategy.backpressure)
+
+      val prj = TestProjection(src, concatenatedRef, _ <= 6)
+
+      // total processing time expected to be around 1.2 seconds
+      projectionTestKit.run(prj, max = 2.seconds) {
+        concatenatedRef.toString shouldBe "1-2-3-4-5-6"
+      }
+    }
+
+    "retry assertion function and fail if timeout expires" in {
+
+      val concatenatedRef = new StringBuffer("")
+
+      // simulate slow stream by adding some delay on each element
+      val src =
+        Source(1 to 20).delayWith(
+          () => DelayStrategy.linearIncreasingDelay(1000.millis, _ => true),
+          DelayOverflowStrategy.backpressure)
+
+      val prj = TestProjection(src, concatenatedRef, _ <= 2)
+
+      assertThrows[TestFailedException] {
+        projectionTestKit.run(prj, max = 1.seconds) {
+          concatenatedRef.toString shouldBe "1-2"
+        }
+      }
+    }
   }
 
-  case class TestProjection(src: Source[Int, NotUsed], probe: TestProbe[Int]) extends Projection[Int] {
+  case class TestProjection(src: Source[Int, NotUsed], concatenatedRef: StringBuffer, concatCond: Int => Boolean)
+      extends Projection[Int] {
 
     override def name: String = "test-projection"
     override def key: String = "00"
@@ -48,8 +82,15 @@ class ProjectionTestKitSpec extends ScalaTestWithActorTestKit with AnyWordSpecLi
     private var shutdown: Option[KillSwitch] = None
     private val promiseToStop = Promise[Done]
 
+    private def concat(elt: Int) = {
+      if (concatenatedRef.toString == "") concatenatedRef.append(elt)
+      else concatenatedRef.append("-" + elt)
+    }
+
     override def processElement(elt: Int): Future[Int] = {
-      probe.ref ! elt
+      if (concatCond(elt)) {
+        concat(elt)
+      }
       Future.successful(elt)
     }
     override def start()(implicit systemProvider: ClassicActorSystemProvider): Unit = {
