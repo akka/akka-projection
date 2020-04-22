ad8c1c8e4494ae9b2926a35eb5e5b37aef3bf494
Patrik Nordwall <patrik.nordwall@gmail.com>
2020-04-22 14:02:15 +0200

    Extract Offset serialization

--- akka-projection-cassandra/src/main/scala/akka/projection/cassandra/internal/CassandraOffsetStore.scala
+++ akka-projection-cassandra/src/main/scala/akka/projection/cassandra/internal/CassandraOffsetStore.scala
@@ -4,34 +4,21 @@
 
 package akka.projection.cassandra.internal
 
-import java.util.UUID
-
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
 import akka.Done
 import akka.annotation.InternalApi
-import akka.persistence.query
 import akka.projection.ProjectionId
+import akka.projection.internal.OffsetSerialization
 import akka.stream.alpakka.cassandra.scaladsl.CassandraSession
-import com.datastax.oss.driver.api.core.cql.Row
-
-/**
- * INTERNAL API
- */
-@InternalApi private[akka] object CassandraOffsetStore {
-  private val StringManifest = "STR"
-  private val LongManifest = "LNG"
-  private val IntManifest = "INT"
-  private val SequenceManifest = "SEQ"
-  private val TimeBasedUUIDManifest = "TBU"
-}
 
 /**
  * INTERNAL API
  */
 @InternalApi private[akka] class CassandraOffsetStore(session: CassandraSession)(implicit ec: ExecutionContext) {
-  import CassandraOffsetStore._
+  import OffsetSerialization.fromStorageRepresentation
+  import OffsetSerialization.toStorageRepresentation
 
   // FIXME make keyspace and table names configurable
   val keyspace = "akka_projection"
@@ -40,53 +27,18 @@ import com.datastax.oss.driver.api.core.cql.Row
   def readOffset[Offset](projectionId: ProjectionId): Future[Option[Offset]] = {
     session
       .selectOne(s"SELECT offset, manifest FROM $keyspace.$table WHERE projection_id = ?", projectionId.id)
-      .map(fromRowToOffset)
+      .map { maybeRow =>
+        maybeRow.map(row => fromStorageRepresentation[Offset](row.getString("offset"), row.getString("manifest")))
+      }
   }
 
   def saveOffset[Offset](projectionId: ProjectionId, offset: Offset): Future[Done] = {
-    val (offsetStr, manifest) = stringOffsetAndManifest(offset)
+    val (offsetStr, manifest) = toStorageRepresentation(offset)
     session.executeWrite(
       s"INSERT INTO $keyspace.$table (projection_id, offset, manifest) VALUES (?, ?, ?)",
       projectionId.id,
       offsetStr,
       manifest)
-  }
-
-  /**
-   * Deserialize an offset from a result row.
-   * The offset is converted from its string representation to its real type.
-   */
-  private def fromRowToOffset[Offset](maybeRow: Option[Row]): Option[Offset] = {
-    maybeRow match {
-      case Some(row) =>
-        val offsetStr = row.getString("offset")
-        val manifest = row.getString("manifest")
-        val offset = (manifest match {
-          case StringManifest        => offsetStr
-          case LongManifest          => offsetStr.toLong
-          case IntManifest           => offsetStr.toInt
-          case SequenceManifest      => query.Offset.sequence(offsetStr.toLong)
-          case TimeBasedUUIDManifest => query.Offset.timeBasedUUID(UUID.fromString(offsetStr))
-        }).asInstanceOf[Offset]
-        Some(offset)
-      case None =>
-        None
-    }
-  }
-
-  /**
-   * Convert the offset to a tuple (String, String) where the first element is
-   * the String representation of the offset and the second its manifest
-   */
-  private def stringOffsetAndManifest[Offset](offset: Offset): (String, String) = {
-    offset match {
-      case s: String                => s -> StringManifest
-      case l: Long                  => l.toString -> LongManifest
-      case i: Int                   => i.toString -> IntManifest
-      case seq: query.Sequence      => seq.value.toString -> SequenceManifest
-      case tbu: query.TimeBasedUUID => tbu.value.toString -> TimeBasedUUIDManifest
-      case _                        => throw new IllegalArgumentException(s"Unsupported offset type, found [${offset.getClass.getName}]")
-    }
   }
 
   def createKeyspaceAndTable(): Future[Done] = {
