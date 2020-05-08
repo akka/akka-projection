088682eeaf2827f3053c9dea6034f9480ef9be87
Sean Glover <sean@seanglover.com>
2020-05-08 15:10:59 -0400

    One actorsystem per kafkaspec

--- akka-projection-kafka/src/test/scala/akka/projection/kafka/KafkaSpecBase.scala
+++ akka-projection-kafka/src/test/scala/akka/projection/kafka/KafkaSpecBase.scala
@@ -4,23 +4,22 @@
 
 package akka.projection.kafka
 
-import akka.actor.testkit.typed.scaladsl.ActorTestKit
-import akka.actor.testkit.typed.scaladsl.ActorTestKitBase
-import akka.actor.testkit.typed.scaladsl.LogCapturing
-import akka.kafka.testkit.scaladsl.ScalatestKafkaSpec
-import akka.kafka.testkit.scaladsl.TestcontainersKafkaLike
+import akka.actor.ActorSystem
+import akka.actor.testkit.typed.scaladsl.{ ActorTestKit, LogCapturing }
+import akka.actor.typed.scaladsl.adapter._
+import akka.kafka.testkit.internal.TestFrameworkInterface
+import akka.kafka.testkit.scaladsl.{ KafkaSpec, TestcontainersKafkaLike }
 import akka.projection.testkit.scaladsl.ProjectionTestKit
-import com.typesafe.config.Config
-import com.typesafe.config.ConfigFactory
-import org.scalatest.OptionValues
-import org.scalatest.concurrent.Eventually
-import org.scalatest.concurrent.PatienceConfiguration
-import org.scalatest.concurrent.ScalaFutures
+import com.typesafe.config.{ Config, ConfigFactory }
+import org.scalatest.concurrent.{ Eventually, PatienceConfiguration, ScalaFutures }
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
+import org.scalatest.{ OptionValues, Suite }
 
 abstract class KafkaSpecBase(val config: Config, kafkaPort: Int)
-    extends ScalatestKafkaSpec(kafkaPort)
+    extends KafkaSpec(kafkaPort, kafkaPort + 1, ActorSystem("Spec", config))
+    with Suite
+    with TestFrameworkInterface.Scalatest
     with AnyWordSpecLike
     with OptionValues
     with LogCapturing
@@ -33,7 +32,7 @@ abstract class KafkaSpecBase(val config: Config, kafkaPort: Int)
   protected def this() = this(config = ConfigFactory.load, kafkaPort = -1)
   protected def this(config: Config) = this(config = config, kafkaPort = -1)
 
-  val testKit = ActorTestKit(ActorTestKitBase.testNameFromCallStack(), config)
+  val testKit = ActorTestKit(system.toTyped)
   val projectionTestKit = new ProjectionTestKit(testKit)
 
   implicit val actorSystem = testKit.system
