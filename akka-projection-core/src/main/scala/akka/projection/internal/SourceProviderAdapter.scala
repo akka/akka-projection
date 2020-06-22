598acc376eb160c9eafc7b99011e166fc2deaac0
Sean Glover <sean@seanglover.com>
2020-06-22 12:12:26 -0400

    revert sourceprovideradapter changes

--- akka-projection-core/src/main/scala/akka/projection/internal/SourceProviderAdapter.scala
+++ akka-projection-core/src/main/scala/akka/projection/internal/SourceProviderAdapter.scala
@@ -4,6 +4,12 @@
 
 package akka.projection.internal
 
+import java.util.Optional
+import java.util.concurrent.CompletionStage
+import java.util.function.Supplier
+
+import scala.compat.java8.FutureConverters._
+import scala.compat.java8.OptionConverters._
 import scala.concurrent.Future
 
 import akka.annotation.InternalApi
@@ -18,7 +24,15 @@ import akka.stream.scaladsl.Source
     delegate: javadsl.SourceProvider[Offset, Envelope])
     extends scaladsl.SourceProvider[Offset, Envelope] {
 
-  def source(offset: () => Future[Option[Offset]]): Future[Source[Envelope, _]] = delegate.toScalaSource(offset)
+  def source(offset: () => Future[Option[Offset]]): Future[Source[Envelope, _]] = {
+    // the parasitic context is used to convert the Optional to Option and a java streams Source to a scala Source,	
+    // it _should_ not be used for the blocking operation of getting offsets themselves	
+    val ec = akka.dispatch.ExecutionContexts.parasitic
+    val offsetAdapter = new Supplier[CompletionStage[Optional[Offset]]] {
+      override def get(): CompletionStage[Optional[Offset]] = offset().map(_.asJava)(ec).toJava
+    }
+    delegate.source(offsetAdapter).toScala.map(_.asScala)(ec)
+  }
 
   def extractOffset(envelope: Envelope): Offset = delegate.extractOffset(envelope)
 }
--- akka-projection-core/src/main/scala/akka/projection/javadsl/SourceProvider.scala
+++ akka-projection-core/src/main/scala/akka/projection/javadsl/SourceProvider.scala
@@ -8,11 +8,6 @@ import java.util.Optional
 import java.util.concurrent.CompletionStage
 import java.util.function.Supplier
 
-import scala.compat.java8.FutureConverters._
-import scala.compat.java8.OptionConverters._
-import scala.concurrent.Future
-
-import akka.annotation.InternalApi
 import akka.projection.MergeableKey
 import akka.projection.MergeableOffset
 import akka.projection.OffsetVerification
@@ -23,17 +18,6 @@ abstract class SourceProvider[Offset, Envelope] {
       offset: Supplier[CompletionStage[Optional[Offset]]]): CompletionStage[akka.stream.javadsl.Source[Envelope, _]]
 
   def extractOffset(envelope: Envelope): Offset
-
-  @InternalApi private[akka] def toScalaSource(
-      offset: () => Future[Option[Offset]]): Future[akka.stream.scaladsl.Source[Envelope, _]] = {
-    // the parasitic context is used to convert the Optional to Option and a java streams Source to a scala Source,
-    // it _should_ not be used for the blocking operation of getting offsets themselves
-    val ec = akka.dispatch.ExecutionContexts.parasitic
-    val offsetAdapter = new Supplier[CompletionStage[Optional[Offset]]] {
-      override def get(): CompletionStage[Optional[Offset]] = offset().map(_.asJava)(ec).toJava
-    }
-    source(offsetAdapter).toScala.map(_.asScala)(ec)
-  }
 
 }
 
