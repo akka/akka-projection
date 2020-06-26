120c5fdaa2b4b4ca1f9d734b543b890857d80ffd
Patrik Nordwall <patrik.nordwall@gmail.com>
2020-06-26 15:38:05 +0200

    fix compilation error in Telemetry
    * and some more minor cleanup there

--- akka-projection-core/src/main/scala/akka/projection/internal/Telemetry.scala
+++ akka-projection-core/src/main/scala/akka/projection/internal/Telemetry.scala
@@ -6,6 +6,7 @@ package akka.projection.internal
 
 import scala.collection.immutable
 
+import akka.NotUsed
 import akka.actor.typed.ActorSystem
 import akka.annotation.InternalApi
 import akka.annotation.InternalStableApi
@@ -15,7 +16,7 @@ import akka.projection.ProjectionId
  * Service Provider Interface (SPI) for collecting metrics from projections.
  *
  * Implementations must include a single constructor with two arguments: [[ProjectionId]]
- * and [[ActorSystem[_]].  To setup your implementation, add a setting on your `application.conf`:
+ * and [[ActorSystem]].  To setup your implementation, add a setting on your `application.conf`:
  *
  * {{{
  * akka.projection.telemetry.implementation-class = com.example.MyMetrics
@@ -25,7 +26,7 @@ import akka.projection.ProjectionId
 trait Telemetry {
 
   /** Invoked when a projection is stopped. The reason for stopping is unspecified, can be a
-   * graceful stop or a failure (see [[failed()]]).
+   * graceful stop or a failure (see [[Telemetry.failed]]).
    */
   def stopped(): Unit
 
@@ -40,9 +41,9 @@ trait Telemetry {
   /**
    * Invoked as soon as the envelope is read, deserialised and ready to be processed.
    *
-   * @param envelope the envelope that's ready for processing.  The type [[Envelope]] will always
+   * @param envelope the envelope that's ready for processing.  The type `Envelope` will always
    *                 represent a single item as stored in the event log.
-   * @return an externally-provided context that will propagate with the envelope until [[afterProcess()]]
+   * @return an externally-provided context that will propagate with the envelope until [[Telemetry.afterProcess]]
    */
   def beforeProcess[Envelope](envelope: Envelope): AnyRef
 
@@ -51,7 +52,7 @@ trait Telemetry {
    * committed).  This method is granted to be invoked after the envelope handler has committed but
    * may or may not happen after the offset was committed (depending on the projection semantics).
    *
-   * @param externalContext the context produced by [[beforeProcess()]] and attached to the processed envelope.
+   * @param externalContext the context produced by [[Telemetry.beforeProcess]] and attached to the processed envelope.
    */
   def afterProcess(externalContext: AnyRef): Unit
 
@@ -66,9 +67,9 @@ trait Telemetry {
   def onOffsetStored(numberOfEnvelopes: Int): Unit
 
   /**
-   * Invoked when processing an envelope errors.  When using a [[HandlerRecoveryStrategy]] that
+   * Invoked when processing an envelope errors.  When using a [[akka.projection.HandlerRecoveryStrategy]] that
    * retries, this method will be invoked as many times as retries.  If the error propagates and
-   * causes the projection to fail [[failed()]] will be invoked.
+   * causes the projection to fail [[Telemetry.failed]] will be invoked.
    *
    * @param cause exception thrown by the errored envelope handler.
    */
@@ -102,7 +103,7 @@ trait Telemetry {
 
   override def stopped(): Unit = {}
 
-  override def beforeProcess[Envelope](envelope: Envelope): AnyRef = Unit
+  override def beforeProcess[Envelope](envelope: Envelope): AnyRef = NotUsed
 
   override def afterProcess(externalContext: AnyRef): Unit = {}
 
