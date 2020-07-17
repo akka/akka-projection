79e77ce069481fc428ab635d5942d525b928f896
Ignasi Marimon-Clos <ignasi@lightbend.com>
2020-07-17 15:43:01 +0200

    DRY

--- examples/src/test/scala/akka/projection/jdbc/docs/OffsetSchemaSpec.scala
+++ examples/src/test/scala/akka/projection/jdbc/docs/OffsetSchemaSpec.scala
@@ -7,6 +7,7 @@ package akka.projection.jdbc.docs
 import scala.io.Source
 
 import akka.projection.jdbc.internal.DefaultDialect
+import akka.projection.jdbc.internal.Dialect
 import akka.projection.jdbc.internal.MSSQLServerDialect
 import akka.projection.jdbc.internal.MySQLDialect
 import akka.projection.jdbc.internal.OracleDialect
@@ -20,53 +21,38 @@ class OffsetSchemaSpec extends AnyWordSpecLike with Matchers {
   "Documentation for JDBC OffsetStore Schema" should {
 
     "match the version used in code (Default - PostgreSQL/H2)" in {
-      val dialectStatements = DefaultDialect(None, "AKKA_PROJECTION_OFFSET_STORE").createTableStatements
-      val fromDialect =
-        s"""
-          |#create-table-default
-          |${dialectStatements.mkString("\n")}
-          |#create-table-default
-          |""".stripMargin
+      val fromDialect = asFileContent(DefaultDialect.apply _, "default")
       val fromSqlFile = Source.fromResource("create-table-default.sql").mkString
       normalize(fromSqlFile) shouldBe normalize(fromDialect)
     }
 
     "match the version used in code (MSSQL Server)" in {
-      val dialectStatements = MSSQLServerDialect(None, "AKKA_PROJECTION_OFFSET_STORE").createTableStatements
-      val fromDialect =
-        s"""
-          |#create-table-mssql
-          |${dialectStatements.mkString("\n")}
-          |#create-table-mssql
-          |""".stripMargin
+      val fromDialect = asFileContent(MSSQLServerDialect.apply _, "mssql")
       val fromSqlFile = Source.fromResource("create-table-mssql.sql").mkString
       normalize(fromSqlFile) shouldBe normalize(fromDialect)
     }
 
     "match the version used in code (MySQL)" in {
-      val dialectStatements = MySQLDialect(None, "AKKA_PROJECTION_OFFSET_STORE").createTableStatements
-      val fromDialect =
-        s"""
-          |#create-table-mysql
-          |${dialectStatements.mkString("\n")}
-          |#create-table-mysql
-          |""".stripMargin
+      val fromDialect = asFileContent(MySQLDialect.apply _, "mysql")
       val fromSqlFile = Source.fromResource("create-table-mysql.sql").mkString
       normalize(fromSqlFile) shouldBe normalize(fromDialect)
     }
 
     "match the version used in code (Oracle)" in {
-      val dialectStatements = OracleDialect(None, "AKKA_PROJECTION_OFFSET_STORE").createTableStatements
-      val fromDialect =
-        s"""
-          |#create-table-oracle
-          |${dialectStatements.mkString("\n")}
-          |#create-table-oracle
-          |""".stripMargin
+      val fromDialect = asFileContent(OracleDialect.apply _, "oracle")
       val fromSqlFile = Source.fromResource("create-table-oracle.sql").mkString
       normalize(fromSqlFile) shouldBe normalize(fromDialect)
     }
 
+  }
+
+  private def asFileContent(dialect: (Option[String], String) => Dialect, dialectCode: String): String = {
+    val dialectStatements = dialect(None, "AKKA_PROJECTION_OFFSET_STORE").createTableStatements
+    s"""
+         |#create-table-$dialectCode
+         |${dialectStatements.mkString("\n")}
+         |#create-table-$dialectCode
+         |""".stripMargin
   }
 
   private def normalize(in: String): String = {
