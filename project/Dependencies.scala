270a0ef5627f88f5d8d3d828b1bb0ab2e65ed19d
Sean Glover <sean@seanglover.com>
2020-07-22 13:38:50 -0400

    Upgrade Alpakka Kafka 2.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   object Versions {
     val akka = sys.props.getOrElse("build.akka.version", "2.6.8")
     val alpakka = "2.0.1"
-    val alpakkaKafka = sys.props.getOrElse("build.alpakka.kafka.version", "2.0.3")
+    val alpakkaKafka = sys.props.getOrElse("build.alpakka.kafka.version", "2.0.4")
     val slick = "3.3.2"
     val scalaTest = "3.1.1"
     val testContainers = "1.14.3"
