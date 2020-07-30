9e39cdb41d53c626de0ffd982ea94e790543ffe5
Sean Glover <sean@seanglover.com>
2020-07-30 15:44:01 -0400

    cleanup, notes

--- project/SequentialTestExecution.scala
+++ project/SequentialTestExecution.scala
@@ -2,26 +2,35 @@ import sbt.Keys._
 import sbt._
 
 /**
- * On travis this plugin Disables parallel execution by limiting 1 task to run at a time.
- * Outside of travis this plugin uses default task concurrency configuration, but disables parallel execution of tests
- * within a task.
+ * On travis this plugin Disables parallel execution by limiting 1 sbt task to run at a time.
+ * Outside of travis this plugin uses sbt's default task concurrency configuration and disables parallel execution of
+ * tests within a task.
  */
 object SequentialTestExecution extends AutoPlugin {
   override def trigger: PluginTrigger = allRequirements
 
   override def projectSettings: Seq[Def.Setting[_]] = {
-    (sys.env
+    sys.env
       .get("TRAVIS") match {
       case Some("true") => Seq(concurrentRestrictions in Global += Tags.limit(Tags.Test, 1))
-      // NOTE:
-      // When `concurrentRestrictions in Global` and `*Test/parallelExecution` are used together it always appears to
-      // invalidate the `concurrentRestriction` setting and run multiple project tests at once (one per task):
-      //
-      //        Seq(
-      //          IntegrationTest / parallelExecution := false,
-      //          Test / parallelExecution := false,
-      //          concurrentRestrictions in Global += Tags.limit(Tags.Test, 1))
+      /*
+          Observations:
+
+          1. `parallelExecution := false` will only run tests in parallel per sbt task. When multiple cores are
+          available sbt's default configuration will run 1 task per sub-project, but each task will run each test method
+          in sequence
+
+          2. When `concurrentRestrictions` and `parallelExecution` are used together sbt always appears to
+          invalidate the `concurrentRestriction` setting and run multiple project tests at once (one per task).
+
+          ```
+          Seq(
+            IntegrationTest / parallelExecution := false,
+            Test / parallelExecution := false,
+            concurrentRestrictions in Global += Tags.limit(Tags.Test, 1))
+          ```
+       */
       case _ => Seq(IntegrationTest / parallelExecution := false, Test / parallelExecution := false)
-    })
+    }
   }
 }
