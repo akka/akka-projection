dff26530db718ad94aaaebd7c8bd0eb9c926f587
Sean Glover <sean@seanglover.com>
2020-12-22 11:48:56 -0500

    Alpakka Kafka 2.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   object Versions {
     val akka = sys.props.getOrElse("build.akka.version", "2.6.10")
     val alpakka = "2.0.2"
-    val alpakkaKafka = sys.props.getOrElse("build.alpakka.kafka.version", "2.1.0-M1")
+    val alpakkaKafka = sys.props.getOrElse("build.alpakka.kafka.version", "2.0.6")
     val slick = "3.3.3"
     val scalaTest = "3.1.1"
     val testContainers = "1.15.1"
