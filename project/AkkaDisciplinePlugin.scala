7688bb94f384c4204ab11f660b12c325f3339220
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-04-20 10:41:40 +0200

    silencer-lib, silencer-plugin 1.7.3 (was 1.7.1) (#535)

--- project/AkkaDisciplinePlugin.scala
+++ project/AkkaDisciplinePlugin.scala
@@ -21,7 +21,7 @@ object AkkaDisciplinePlugin extends AutoPlugin {
   val nonFatalWarningsFor = Set("docs")
 
   lazy val silencerSettings = {
-    val silencerVersion = "1.7.1"
+    val silencerVersion = "1.7.3"
     Seq(
       libraryDependencies ++= Seq(
           compilerPlugin(("com.github.ghik" %% "silencer-plugin" % silencerVersion).cross(CrossVersion.patch)),
