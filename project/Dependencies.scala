3064c84a6f1024ec10ea58c0a6ed1189365f8ffb
Scala Steward <me@scala-steward.org>
2021-05-17 04:04:17 +0200

    mysql-connector-java 8.0.25 (was 8.0.23)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
 
     val h2Driver = Compile.h2Driver % allTestConfig
     val postgresDriver = "org.postgresql" % "postgresql" % "42.2.19" % allTestConfig
-    val mysqlDriver = "mysql" % "mysql-connector-java" % "8.0.23" % allTestConfig
+    val mysqlDriver = "mysql" % "mysql-connector-java" % "8.0.25" % allTestConfig
     val msSQLServerDriver = "com.microsoft.sqlserver" % "mssql-jdbc" % "7.4.1.jre8" % allTestConfig
     val oracleDriver = "com.oracle.ojdbc" % "ojdbc8" % "19.3.0.0" % allTestConfig
 
