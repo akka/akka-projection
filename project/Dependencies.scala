14942303575b3fe19ae2b3bdeb8446180e5c4b18
Scala Steward <me@scala-steward.org>
2021-09-14 12:04:07 +0200

    akka-persistence-jdbc 5.0.4 (was 5.0.2)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
 
   object Versions {
     val akka = sys.props.getOrElse("build.akka.version", "2.6.16")
-    val akkaPersistenceJdbc = "5.0.2"
+    val akkaPersistenceJdbc = "5.0.4"
     val alpakka = "2.0.2"
     val alpakkaKafka = sys.props.getOrElse("build.alpakka.kafka.version", "2.0.7")
     val slick = "3.3.3"
