4123811847f00a3ec3553580e0b06333564fe07a
Scala Steward <me@scala-steward.org>
2022-05-13 03:03:21 +0200

    postgresql 42.3.5 (was 42.3.1)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -63,7 +63,7 @@ object Dependencies {
     val junit = "junit" % "junit" % Versions.junit % allTestConfig
 
     val h2Driver = Compile.h2Driver % allTestConfig
-    val postgresDriver = "org.postgresql" % "postgresql" % "42.3.1" % allTestConfig
+    val postgresDriver = "org.postgresql" % "postgresql" % "42.3.5" % allTestConfig
     val mysqlDriver = "mysql" % "mysql-connector-java" % "8.0.28" % allTestConfig
     val msSQLServerDriver = "com.microsoft.sqlserver" % "mssql-jdbc" % "7.4.1.jre8" % allTestConfig
     val oracleDriver = "com.oracle.ojdbc" % "ojdbc8" % "19.3.0.0" % allTestConfig
