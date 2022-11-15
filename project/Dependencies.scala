9687a090636cc731ae644e3a96dcbfe196deaad6
Patrik Nordwall <patrik.nordwall@gmail.com>
2022-11-15 20:46:07 +0100

    bump: akka-persistence-r2dbc 1.0.1 (#745)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
     val akka = sys.props.getOrElse("build.akka.version", "2.7.0")
     val akkaPersistenceCassandra = "1.1.0"
     val akkaPersistenceJdbc = "5.2.0"
-    val akkaPersistenceR2dbc = "1.0.0"
+    val akkaPersistenceR2dbc = "1.0.1"
     val alpakka = "5.0.0"
     val alpakkaKafka = sys.props.getOrElse("build.alpakka.kafka.version", "4.0.0")
     val slick = "3.4.1"
