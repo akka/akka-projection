d48361bb2bd2a16653b406eecfc8f2769b9d250a
Johan Andrén <johan@markatta.com>
2023-01-26 09:15:43 +0100

    feat: Convenience factories for using with Akka Projection gRPC (#352)

---
+++ akka-projection-r2dbc/src/main/scala/akka/projection/r2dbc/scaladsl/R2dbcReplication.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2022 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.projection.r2dbc.scaladsl
+
+import akka.actor.typed.ActorSystem
+import akka.projection.grpc.replication.scaladsl.ReplicationProjectionProvider
+import akka.projection.r2dbc.R2dbcProjectionSettings
+
+object R2dbcReplication {
+
+  /**
+   * Creates a projection provider for using R2dbc as backend for the Akka Projection gRPC transport for Replicated
+   * Event Sourcing.
+   */
+  def apply()(implicit system: ActorSystem[_]): ReplicationProjectionProvider =
+    R2dbcProjection.atLeastOnceFlow(
+      _,
+      Some(R2dbcProjectionSettings(system).withWarnAboutFilteredEventsInFlow(false)),
+      _,
+      _)(_)
+
+  /**
+   * Creates a projection provider for using R2dbc as backend for the Akka Projection gRPC transport for Replicated
+   * Event Sourcing.
+   */
+  def apply(settings: R2dbcProjectionSettings)(implicit system: ActorSystem[_]): ReplicationProjectionProvider =
+    R2dbcProjection.atLeastOnceFlow(_, Some(settings.withWarnAboutFilteredEventsInFlow(false)), _, _)(_)
+
+}
