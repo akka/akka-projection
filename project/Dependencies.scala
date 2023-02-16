c7314f36d62923264d951b1becc50e4d0c2dd2e8
Scala Steward <scala_steward@virtuslab.com>
2023-02-16 13:27:09 +0000

    bump: hibernate-core 5.6.15.Final (was 5.6.14.Final)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -105,7 +105,7 @@ object Dependencies {
   }
 
   object Examples {
-    val hibernate = "org.hibernate" % "hibernate-core" % "5.6.14.Final"
+    val hibernate = "org.hibernate" % "hibernate-core" % "5.6.15.Final"
 
     val akkaPersistenceTyped = "com.typesafe.akka" %% "akka-persistence-typed" % Versions.akka
     val akkaClusterShardingTyped = "com.typesafe.akka" %% "akka-cluster-sharding-typed" % Versions.akka
