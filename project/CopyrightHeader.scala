89108ff199b6aaed1ddcc61ae7ab88cb028ca8fc
Johan Andrén <johan@markatta.com>
2023-03-14 15:03:10 +0100

    chore: Bump (c) to 2023

--- project/CopyrightHeader.scala
+++ project/CopyrightHeader.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2021 Lightbend Inc. <https://www.lightbend.com>
+ * Copyright (C) 2020-2023 Lightbend Inc. <https://www.lightbend.com>
  */
 import de.heikoseeberger.sbtheader.CommentCreator
 import de.heikoseeberger.sbtheader.HeaderPlugin
@@ -23,7 +23,7 @@ object CopyrightHeader extends AutoPlugin {
 
   // We hard-code this so PR's created in year X will not suddenly fail in X+1.
   // Of course we should remember to update it early in the year.
-  val CurrentYear = "2022"
+  val CurrentYear = "2023"
   val CopyrightPattern = "Copyright \\([Cc]\\) (\\d{4}(-\\d{4})?) (Lightbend|Typesafe) Inc. <.*>".r
   val CopyrightHeaderPattern = s"(?s).*${CopyrightPattern}.*".r
 
