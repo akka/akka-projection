6ac443cdfa5563c1b366cdd042a6fa7e8e3f052b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-06-26 08:47:09 +0200

    bump: scala-library 2.12.18 (was 2.12.17) (#924)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ import sbt._
 object Dependencies {
 
   val Scala213 = "2.13.10"
-  val Scala212 = "2.12.17"
+  val Scala212 = "2.12.18"
   val Scala3 = "3.2.2"
   val Scala2Versions = Seq(Scala213, Scala212)
   val ScalaVersions = Dependencies.Scala2Versions :+ Dependencies.Scala3
