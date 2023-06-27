3313215b13fe2411b01696183d481dbe8816a6e7
Johan Andrén <johan@markatta.com>
2023-06-27 15:19:03 +0200

    Producer filtering of events, first stab

--- akka-projection-grpc/src/main/scala/akka/persistence/EventWriter.scala
+++ akka-projection-grpc/src/main/scala/akka/persistence/EventWriter.scala
@@ -7,6 +7,7 @@ package akka.persistence
 import akka.Done
 import akka.actor.typed.ActorRef
 import akka.actor.typed.Behavior
+import akka.actor.typed.SupervisorStrategy
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.LoggerOps
 import akka.actor.typed.scaladsl.adapter.TypedActorRefOps
@@ -35,83 +36,89 @@ private[akka] object EventWriter {
 
   def apply(journalPluginId: String): Behavior[Command] =
     Behaviors
-      .setup[AnyRef] { context =>
-        val writerUuid = UUID.randomUUID().toString
-        val journal = Persistence(context.system).journalFor(journalPluginId)
-        context.log.debug("Event writer for journal [{}] starting up", journalPluginId)
+      .supervise(Behaviors
+        .setup[AnyRef] { context =>
+          val writerUuid = UUID.randomUUID().toString
+          val journal = Persistence(context.system).journalFor(journalPluginId)
+          context.log.debug("Event writer for journal [{}] starting up", journalPluginId)
 
-        val waitingForResponse = new util.HashMap[(String, Long), ActorRef[StatusReply[Done]]]()
+          val waitingForResponse = new util.HashMap[(String, Long), ActorRef[StatusReply[Done]]]()
 
-        Behaviors.receiveMessage {
-          case Write(persistenceId, sequenceNumber, event, metadata, replyTo) =>
-            // FIXME trace or remove
-            context.log.debug("Writing event persistence id [{}], sequence nr [{}]", persistenceId, sequenceNumber)
-            val repr = PersistentRepr(
-              event,
-              persistenceId = persistenceId,
-              sequenceNr = sequenceNumber,
-              manifest = "", // adapters would be on the producing side, already applied
-              writerUuid = writerUuid,
-              sender = akka.actor.ActorRef.noSender)
+          Behaviors.receiveMessage {
+            case Write(persistenceId, sequenceNumber, event, metadata, replyTo) =>
+              if (context.log.isTraceEnabled)
+                context.log.traceN(
+                  "Writing event persistence id [{}], sequence nr [{}], payload {}",
+                  persistenceId,
+                  sequenceNumber,
+                  event)
+              val repr = PersistentRepr(
+                event,
+                persistenceId = persistenceId,
+                sequenceNr = sequenceNumber,
+                manifest = "", // adapters would be on the producing side, already applied
+                writerUuid = writerUuid,
+                sender = akka.actor.ActorRef.noSender)
 
-            val write = AtomicWrite(metadata match {
-                case Some(meta) => repr.withMetadata(meta)
-                case _          => repr
-              }) :: Nil
+              val write = AtomicWrite(metadata match {
+                  case Some(meta) => repr.withMetadata(meta)
+                  case _          => repr
+                }) :: Nil
 
-            waitingForResponse.put((persistenceId, sequenceNumber), replyTo)
+              waitingForResponse.put((persistenceId, sequenceNumber), replyTo)
 
-            journal ! JournalProtocol.WriteMessages(write, context.self.toClassic, context.self.path.uid)
-            Behaviors.same
+              journal ! JournalProtocol.WriteMessages(write, context.self.toClassic, context.self.path.uid)
+              Behaviors.same
 
-          case JournalProtocol.WriteMessageSuccess(message, _) =>
-            val pidSeqnr = (message.persistenceId, message.sequenceNr)
-            waitingForResponse.get(pidSeqnr) match {
-              case null =>
-                context.log.warn2(
-                  "Got write success reply for event with no waiting request, probably a bug (pid {}, seq nr {})",
-                  message.persistenceId,
-                  message.sequenceNr)
-                Behaviors.same
-              case replyTo =>
-                if (context.log.isTraceEnabled)
-                  context.log.trace2(
-                    "Successfully wrote event persistence id [{}], sequence nr [{}]",
+            case JournalProtocol.WriteMessageSuccess(message, _) =>
+              val pidSeqnr = (message.persistenceId, message.sequenceNr)
+              waitingForResponse.get(pidSeqnr) match {
+                case null =>
+                  context.log.warn2(
+                    "Got write success reply for event with no waiting request, probably a bug (pid {}, seq nr {})",
                     message.persistenceId,
                     message.sequenceNr)
-                replyTo ! StatusReply.success(Done)
-                waitingForResponse.remove(pidSeqnr)
-                Behaviors.same
-            }
-            Behaviors.same
+                  Behaviors.same
+                case replyTo =>
+                  if (context.log.isTraceEnabled)
+                    context.log.trace2(
+                      "Successfully wrote event persistence id [{}], sequence nr [{}]",
+                      message.persistenceId,
+                      message.sequenceNr)
+                  replyTo ! StatusReply.success(Done)
+                  waitingForResponse.remove(pidSeqnr)
+                  Behaviors.same
+              }
+              Behaviors.same
 
-          case JournalProtocol.WriteMessageFailure(message, error, _) =>
-            val pidSeqnr = (message.persistenceId, message.sequenceNr)
-            waitingForResponse.get(pidSeqnr) match {
-              case null =>
-                context.log.warnN(
-                  s"Got error reply for event with no waiting request, probably a bug (pid ${message.persistenceId}, seq nr ${message.sequenceNr})",
-                  error)
-                Behaviors.same
-              case replyTo =>
-                context.log.warnN(
-                  "Failed writing event persistence id [{}], sequence nr [{}]: {}",
-                  message.persistenceId,
-                  message.sequenceNr,
-                  error.getMessage)
+            case JournalProtocol.WriteMessageFailure(message, error, _) =>
+              val pidSeqnr = (message.persistenceId, message.sequenceNr)
+              waitingForResponse.get(pidSeqnr) match {
+                case null =>
+                  context.log.warnN(
+                    s"Got error reply for event with no waiting request, probably a bug (pid ${message.persistenceId}, seq nr ${message.sequenceNr})",
+                    error)
+                  Behaviors.same
+                case replyTo =>
+                  context.log.warnN(
+                    "Failed writing event persistence id [{}], sequence nr [{}]: {}",
+                    message.persistenceId,
+                    message.sequenceNr,
+                    error.getMessage)
 
-                replyTo ! StatusReply.error(error.getMessage)
+                  replyTo ! StatusReply.error(error.getMessage)
 
-                waitingForResponse.remove(pidSeqnr)
-                Behaviors.same
-            }
+                  waitingForResponse.remove(pidSeqnr)
+                  Behaviors.same
+              }
 
-          case _ =>
-            // ignore all other journal protocol messages
-            Behaviors.same
+            case _ =>
+              // ignore all other journal protocol messages
+              Behaviors.same
 
-        }
-      }
+          }
+        })
+      .onFailure[Exception](SupervisorStrategy.restart)
       .narrow[Command]
 
 }
