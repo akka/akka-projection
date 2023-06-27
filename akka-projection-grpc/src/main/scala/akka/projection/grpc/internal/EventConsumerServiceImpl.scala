e200c1b67ea1c4b933143f23efe466fc970a3c30
Johan Andrén <johan@markatta.com>
2023-06-27 10:13:46 +0200

    Persistence id transformation

--- akka-projection-grpc/src/main/scala/akka/projection/grpc/internal/EventConsumerServiceImpl.scala
+++ akka-projection-grpc/src/main/scala/akka/projection/grpc/internal/EventConsumerServiceImpl.scala
@@ -5,6 +5,7 @@
 package akka.projection.grpc.internal
 
 import akka.Done
+import akka.actor.typed.ActorRef
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.scaladsl.AskPattern.schedulerFromActorSystem
@@ -29,16 +30,14 @@ import scala.concurrent.duration.DurationInt
  */
 private[akka] object EventConsumerServiceImpl {
 
-  def directJournalConsumer(journalPluginId: String)(implicit system: ActorSystem[_]): EventConsumerServiceImpl = {
+  def directJournalConsumer(journalPluginId: String, persistenceIdTransformer: String => String)(
+      implicit system: ActorSystem[_]): EventConsumerServiceImpl = {
+    // FIXME is this name unique, could we create multiple for the same journal? (we wouldn't be able to bind them to the same port)
     val eventWriter = system.systemActorOf(
       EventWriter(journalPluginId),
       s"EventWriter-${URLEncoder.encode(journalPluginId, ByteString.UTF_8)}")
-    implicit val timeout: Timeout = 5.seconds // FIXME from config or can we get rid of it
 
-    val writeEventToJournal: EventEnvelope[Any] => Future[Done] = envelope =>
-      eventWriter.askWithStatus[Done](replyTo => EventWriter.Write(envelope, replyTo))
-
-    new EventConsumerServiceImpl(writeEventToJournal)
+    new EventConsumerServiceImpl(eventWriter, persistenceIdTransformer)
   }
 
 }
@@ -46,18 +45,30 @@ private[akka] object EventConsumerServiceImpl {
 /**
  * INTERNAL API
  */
-private[akka] final class EventConsumerServiceImpl(consumeEvent: EventEnvelope[Any] => Future[Done])(
-    implicit system: ActorSystem[_])
+private[akka] final class EventConsumerServiceImpl(
+    eventWriter: ActorRef[EventWriter.Command],
+    persistenceIdTransformer: String => String)(implicit system: ActorSystem[_])
     extends EventConsumerService {
   private val logger = LoggerFactory.getLogger(classOf[EventConsumerServiceImpl])
 
   private val protoAnySerialization = new ProtoAnySerialization(system)
+
+  implicit val timeout: Timeout = 5.seconds // FIXME from config or can we get rid of it
+  private def writeEventToJournal(envelope: EventEnvelope[Any]): Future[Done] = {
+    // FIXME would we want request metadata/producer ip/entire envelope in to persistence id transformer?
+    val persistenceId = persistenceIdTransformer(envelope.persistenceId)
+
+    // FIXME can we skip the ask for each event?
+    eventWriter.askWithStatus[Done](replyTo =>
+      EventWriter.Write(persistenceId, envelope.sequenceNr, envelope.event, envelope.eventMetadata, replyTo))
+  }
+
   override def consumeEvent(in: Event): Future[Empty] = {
     // FIXME do we need to make sure events for the same pid are ordered? Should be single writer per pid but?
     val envelope = ProtobufProtocolConversions.eventToEnvelope[Any](in, protoAnySerialization)
-    logger.debug("Saw event [{}] for pid [{}]", envelope.sequenceNr, envelope.persistenceId)
+    logger.trace("Saw event [{}] for pid [{}]", envelope.sequenceNr, envelope.persistenceId)
 
-    consumeEvent(envelope).map(_ => Empty.defaultInstance)(ExecutionContexts.parasitic)
+    writeEventToJournal(envelope).map(_ => Empty.defaultInstance)(ExecutionContexts.parasitic)
   }
 
 }
