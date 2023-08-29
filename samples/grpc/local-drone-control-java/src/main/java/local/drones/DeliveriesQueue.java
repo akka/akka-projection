feae2fe37258d59cf8fbeeac9d7fa6bdd40e9cb6
Johan Andrén <johan@markatta.com>
2023-08-29 17:24:14 +0200

    docs: Java version of drone sample started

---
+++ samples/grpc/local-drone-control-java/src/main/java/local/drones/DeliveriesQueue.java
@@ -0,0 +1,222 @@
+package local.drones;
+
+import akka.Done;
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.BackoffSupervisorStrategy;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.cluster.sharding.typed.javadsl.EntityTypeKey;
+import akka.pattern.StatusReply;
+import akka.persistence.typed.PersistenceId;
+import akka.persistence.typed.state.javadsl.CommandHandler;
+import akka.persistence.typed.state.javadsl.DurableStateBehavior;
+import akka.persistence.typed.state.javadsl.Effect;
+import com.google.rpc.context.AttributeContext;
+
+import java.time.Instant;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static akka.Done.done;
+
+public final class DeliveriesQueue extends DurableStateBehavior<DeliveriesQueue.Command, DeliveriesQueue.State> {
+
+    public interface Command extends CborSerializable {
+    }
+
+    public static final class AddDelivery implements Command {
+        public final WaitingDelivery delivery;
+        public final ActorRef<Done> replyTo;
+
+        public AddDelivery(WaitingDelivery delivery, ActorRef<Done> replyTo) {
+            this.delivery = delivery;
+            this.replyTo = replyTo;
+        }
+    }
+
+    public static final class RequestDelivery implements Command {
+        public final String droneId;
+        public final Coordinates droneCoordinates;
+        public final ActorRef<StatusReply<WaitingDelivery>> replyTo;
+
+        public RequestDelivery(String droneId, Coordinates droneCoordinates, ActorRef<StatusReply<WaitingDelivery>> replyTo) {
+            this.droneId = droneId;
+            this.droneCoordinates = droneCoordinates;
+            this.replyTo = replyTo;
+        }
+    }
+
+    public static final class CompleteDelivery implements Command {
+            public final String deliveryId;
+            public final ActorRef<StatusReply<Done>> replyTo;
+
+        public CompleteDelivery(String deliveryId, ActorRef<StatusReply<Done>> replyTo) {
+            this.deliveryId = deliveryId;
+            this.replyTo = replyTo;
+        }
+    }
+
+    public static final class GetCurrentState implements Command {
+        public final ActorRef<State> replyTo;
+
+        public GetCurrentState(ActorRef<State> replyTo) {
+            this.replyTo = replyTo;
+        }
+    }
+
+
+
+
+    final class State {
+        public final List<WaitingDelivery> waitingDeliveries;
+        public final List<DeliveryInProgress> deliveriesInProgress;
+
+        public State() {
+            waitingDeliveries = new ArrayList<>();
+            deliveriesInProgress = new ArrayList<>();
+        }
+
+        public State(List<WaitingDelivery> waitingDeliveries, List<DeliveryInProgress> deliveriesInProgress) {
+            this.waitingDeliveries = waitingDeliveries;
+            this.deliveriesInProgress = deliveriesInProgress;
+        }
+    }
+
+    public static final class WaitingDelivery {
+        public final String deliveryId;
+        public final Coordinates from;
+        public final Coordinates to;
+
+        public WaitingDelivery(String deliveryId, Coordinates from, Coordinates to) {
+            this.deliveryId = deliveryId;
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            WaitingDelivery that = (WaitingDelivery) o;
+
+            if (!deliveryId.equals(that.deliveryId)) return false;
+            if (!from.equals(that.from)) return false;
+            return to.equals(that.to);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = deliveryId.hashCode();
+            result = 31 * result + from.hashCode();
+            result = 31 * result + to.hashCode();
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "WaitingDelivery{" +
+                    "deliveryId='" + deliveryId + '\'' +
+                    ", from=" + from +
+                    ", to=" + to +
+                    '}';
+        }
+    }
+
+    final class DeliveryInProgress {
+        public final String deliveryId;
+        public final String droneId;
+        public final Instant pickupTime;
+
+        public DeliveryInProgress(String deliveryId, String droneId, Instant pickupTime) {
+            this.deliveryId = deliveryId;
+            this.droneId = droneId;
+            this.pickupTime = pickupTime;
+        }
+    }
+
+    // Not really an entity, we just have a single instance
+    static public final EntityTypeKey<Command> EntityKey = EntityTypeKey.create(Command.class, "RestaurantDeliveries");
+
+    public static Behavior<Command> create() {
+        return Behaviors.setup(context ->
+                new DeliveriesQueue(context, PersistenceId.of(EntityKey.name(), "DeliveriesQueue")));
+
+    }
+
+    private final ActorContext<Command> context;
+
+    public DeliveriesQueue(ActorContext<Command> context, PersistenceId persistenceId) {
+        super(persistenceId);
+        this.context = context;
+    }
+
+    @Override
+    public State emptyState() {
+        return new State();
+    }
+
+    @Override
+    public CommandHandler<Command, State> commandHandler() {
+        return newCommandHandlerBuilder()
+                .forAnyState()
+                .onCommand(AddDelivery.class, this::onAddDelivery)
+                .onCommand(RequestDelivery.class, this::onRequestDelivery)
+                .onCommand(CompleteDelivery.class, this::onCompleteDelivery)
+                .onCommand(GetCurrentState.class, this::onGetCurrentState)
+                .build();
+    }
+
+    private Effect<State> onAddDelivery(State state, AddDelivery command) {
+        context.getLog().info("Adding delivery [{}] to queue", command.delivery.deliveryId);
+        if (state.waitingDeliveries.contains(command.delivery) || state.deliveriesInProgress.stream().anyMatch(deliveryInProgress ->
+                deliveryInProgress.deliveryId.equals(command.delivery.deliveryId)))
+            return Effect().reply(command.replyTo, done());
+        else {
+            state.waitingDeliveries.add(command.delivery);
+            return Effect()
+                    .persist(state)
+                    .thenReply(command.replyTo, updatedState -> done());
+        }
+    }
+
+    private Effect<State> onRequestDelivery(State state, RequestDelivery command) {
+        if (state.waitingDeliveries.isEmpty()) {
+            return Effect().reply(command.replyTo, StatusReply.error("No waiting orders"));
+        } else {
+            var closestPickupForDrone = state.waitingDeliveries.stream().min(Comparator.comparingLong(delivery ->
+                            command.droneCoordinates.distanceTo(delivery.from)))
+                    .get();
+            context.getLog().info(
+                    "Selected next delivery [{}] for drone [{}]",
+                    closestPickupForDrone.deliveryId,
+                    command.droneId);
+            // Note: A real application would have to care more about retries/lost data here
+            state.waitingDeliveries.remove(closestPickupForDrone);
+            state.deliveriesInProgress.add(new DeliveryInProgress(closestPickupForDrone.deliveryId, command.droneId, Instant.now()));
+            return Effect().persist(state)
+                    .thenReply(command.replyTo, updatedState -> StatusReply.success(closestPickupForDrone));
+        }
+    }
+
+    private Effect<State> onCompleteDelivery(State state, CompleteDelivery command) {
+        var maybeExisting = state.deliveriesInProgress.stream().filter(delivery -> delivery.deliveryId.equals(command.deliveryId)).findFirst();
+        if (maybeExisting.isEmpty()) {
+            return Effect().reply(command.replyTo, StatusReply.error("Unknown delivery id: " + command.deliveryId));
+        } else {
+            var existing = maybeExisting.get();
+            state.deliveriesInProgress.remove(existing);
+
+            return Effect()
+                    .persist(state)
+                    .thenReply(command.replyTo, updatedState -> StatusReply.success(done()));
+        }
+    }
+
+    private Effect<State> onGetCurrentState(State state, GetCurrentState command) {
+        // defensive copy since state is mutable (individual values in the lists are immutable)
+        var stateToShare = new State(new ArrayList<>(state.waitingDeliveries), new ArrayList<>(state.deliveriesInProgress));
+        return Effect().reply(command.replyTo, stateToShare);
+    }
+}
---
+++ samples/grpc/local-drone-control-java/src/main/java/local/drones/Drone.java
@@ -0,0 +1,167 @@
+package local.drones;
+
+import akka.Done;
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.Behavior;
+import akka.cluster.sharding.typed.javadsl.ClusterSharding;
+import akka.cluster.sharding.typed.javadsl.EntityTypeKey;
+import akka.cluster.sharding.typed.javadsl.Entity;
+import akka.pattern.StatusReply;
+import akka.persistence.typed.PersistenceId;
+import akka.persistence.typed.javadsl.CommandHandler;
+import akka.persistence.typed.javadsl.Effect;
+import akka.persistence.typed.javadsl.EventHandler;
+import akka.persistence.typed.javadsl.EventSourcedBehavior;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static akka.Done.done;
+
+public class Drone extends EventSourcedBehavior<Drone.Command, Drone.Event, Drone.State> {
+
+    interface Command extends CborSerializable {}
+
+    public static final class ReportPosition implements Command {
+        public final Position position;
+        public final ActorRef<Done> replyTo;
+
+        public ReportPosition(Position position, ActorRef<Done> replyTo) {
+            this.position = position;
+            this.replyTo = replyTo;
+        }
+    }
+
+    public static final class GetCurrentPosition implements Command {
+        public final ActorRef<StatusReply<Position>> replyTo;
+
+        public GetCurrentPosition(ActorRef<StatusReply<Position>> replyTo) {
+            this.replyTo = replyTo;
+        }
+    }
+
+
+    interface Event extends CborSerializable {}
+
+    public static final class PositionUpdated implements Event {
+        public final Position position;
+
+        public PositionUpdated(Position position) {
+            this.position = position;
+        }
+    }
+
+    public static final class CoarseGrainedLocationChanged implements Event {
+            public final CoarseGrainedCoordinates coordinates;
+
+        public CoarseGrainedLocationChanged(CoarseGrainedCoordinates coordinates) {
+            this.coordinates = coordinates;
+        }
+    }
+
+
+
+    class State implements CborSerializable {
+        Optional<Position> currentPosition;
+        final List<Position> historicalPositions;
+
+        State() {
+            currentPosition = Optional.empty();
+            historicalPositions = new ArrayList<>();
+        }
+
+        Optional<CoarseGrainedCoordinates> coarseGrainedCoordinates() {
+            return currentPosition.map(p ->
+                    CoarseGrainedCoordinates.fromCoordinates(p.coordinates));
+        }
+
+    }
+
+    public static final EntityTypeKey<Command> ENTITY_KEY = EntityTypeKey.create(Command.class, "Drone");
+
+    private static final int LOCATION_HISTORY_LIMIT = 100;
+
+    public static void init(ActorSystem<?> system) {
+        ClusterSharding.get(system).init(Entity.of(ENTITY_KEY, entityContext ->
+                new Drone(entityContext.getEntityId())));
+    }
+
+    public static Behavior<Command> create(String entityId) {
+        return new Drone(entityId);
+    }
+
+    private Drone(String entityId) {
+        super(PersistenceId.of(ENTITY_KEY.name(), entityId));
+    }
+
+    @Override
+    public State emptyState() {
+        return new State();
+    }
+
+    @Override
+    public CommandHandler<Command, Event, State> commandHandler() {
+        return newCommandHandlerBuilder().forAnyState()
+                .onCommand(ReportPosition.class, this::onReportPosition)
+                .onCommand(GetCurrentPosition.class, this::onGetCurrentPosition)
+                .build();
+    }
+
+
+
+    @Override
+    public EventHandler<State, Event> eventHandler() {
+        return newEventHandlerBuilder()
+                .forAnyState()
+                .onEvent(PositionUpdated.class,
+                        (state, event) -> {
+                            if (state.currentPosition.isPresent()) {
+                                state.historicalPositions.add(state.currentPosition.get());
+                                if (state.historicalPositions.size() > LOCATION_HISTORY_LIMIT) {
+                                    state.historicalPositions.remove(0);
+                                }
+                            }
+                            state.currentPosition = Optional.of(event.position);
+                            return state;
+                        })
+                .onEvent(CoarseGrainedLocationChanged.class, (state, event) ->
+                        // can be derived from position, so not really updating state,
+                        // persisted as events for aggregation
+                        state)
+                .build();
+    }
+
+    private Effect<Event, State> onReportPosition(State state, ReportPosition command) {
+        if (state.currentPosition.equals(Optional.of(command.position))) {
+            // already seen
+            return Effect().reply(command.replyTo, done());
+        } else {
+            var newCoarseGrainedLocation = CoarseGrainedCoordinates.fromCoordinates(command.position.coordinates);
+            if (state.coarseGrainedCoordinates().equals(Optional.of(newCoarseGrainedLocation))) {
+                // same grid location as before
+                return Effect()
+                        .persist(new PositionUpdated(command.position))
+                        .thenReply(command.replyTo, newState -> done());
+            } else {
+                // no previous location known or new grid location
+                return Effect()
+                        .persist(
+                                Arrays.asList(
+                                new PositionUpdated(command.position),
+                                new CoarseGrainedLocationChanged(newCoarseGrainedLocation)))
+                        .thenReply(command.replyTo, newState -> done());
+            }
+        }
+    }
+
+    private Effect<Event, State> onGetCurrentPosition(State state, GetCurrentPosition command) {
+        return state.currentPosition.map(position -> Effect().reply(command.replyTo, StatusReply.success(position)))
+                .orElse(Effect().reply(command.replyTo, StatusReply.error("Position of drone is unknown"))
+        );
+    }
+
+
+}
