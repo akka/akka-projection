a3ad649c9a5c21104bad40f76f664b43146e67b8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-15 16:51:03 +0200

    bump: scala3-library 3.3.1 (was 3.2.2) (#926)
    ---------
    
    Co-authored-by: Johan Andrén <johan@markatta.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,8 +7,8 @@ object Dependencies {
 
   val Scala213 = "2.13.11"
   val Scala212 = "2.12.18"
+  val Scala3 = "3.3.1"
 
-  val Scala3 = "3.2.2"
   val Scala2Versions = Seq(Scala213, Scala212)
   val ScalaVersions = Dependencies.Scala2Versions :+ Dependencies.Scala3
 
