9e355aeccf0fea6fd996ced2e8bb071f6d85127f
Patrik Nordwall <patrik.nordwall@gmail.com>
2023-09-21 08:21:39 +0200

    feat: Add R2dbcSession.withSession (#1017)
    * corresponding to akka.persistence.r2dbc.session..R2dbcSession
    * also noticed that javadsl had wrong function type, ApiMayChange
      so just changed, and removed ApiMayChange

--- akka-projection-r2dbc/src/main/scala/akka/projection/r2dbc/scaladsl/R2dbcSession.scala
+++ akka-projection-r2dbc/src/main/scala/akka/projection/r2dbc/scaladsl/R2dbcSession.scala
@@ -7,12 +7,53 @@ package akka.projection.r2dbc.scaladsl
 import scala.collection.immutable
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
 import akka.actor.typed.ActorSystem
+import akka.annotation.InternalApi
+import akka.persistence.r2dbc.ConnectionFactoryProvider
+import akka.persistence.r2dbc.ConnectionPoolSettings
 import akka.persistence.r2dbc.internal.R2dbcExecutor
+import akka.projection.r2dbc.R2dbcProjectionSettings
 import io.r2dbc.spi.Connection
 import io.r2dbc.spi.Row
 import io.r2dbc.spi.Statement
+import org.slf4j.LoggerFactory
+
+object R2dbcSession {
+  private val log = LoggerFactory.getLogger(classOf[R2dbcSession])
+
+  private val logDbCallsDisabled = -1.millis
+
+  /**
+   * Runs the passed function in using a R2dbcSession with a new transaction. The connection is closed and the
+   * transaction is committed at the end or rolled back in case of failures.
+   */
+  def withSession[A](system: ActorSystem[_])(fun: R2dbcSession => Future[A]): Future[A] = {
+    withSession(system, connectionFactoryConfigPath(system))(fun)
+  }
+
+  def withSession[A](system: ActorSystem[_], connectionFactoryConfigPath: String)(
+      fun: R2dbcSession => Future[A]): Future[A] = {
+    val connectionFactoryProvider = ConnectionFactoryProvider(system)
+    val connectionFactory = connectionFactoryProvider.connectionFactoryFor(connectionFactoryConfigPath)
+    val poolSettings = new ConnectionPoolSettings(system.settings.config.getConfig(connectionFactoryConfigPath))
+    val r2dbcExecutor =
+      new R2dbcExecutor(connectionFactory, log, logDbCallsDisabled, poolSettings.closeCallsExceeding)(
+        system.executionContext,
+        system)
+    r2dbcExecutor.withConnection("R2dbcSession") { connection =>
+      val session = new R2dbcSession(connection)(system.executionContext, system)
+      fun(session)
+    }
+  }
+
+  /**
+   * INTERNAL API
+   */
+  @InternalApi private[akka] def connectionFactoryConfigPath(system: ActorSystem[_]): String =
+    system.settings.config.getConfig(R2dbcProjectionSettings.DefaultConfigPath).getString("use-connection-factory")
+}
 
 final class R2dbcSession(val connection: Connection)(implicit val ec: ExecutionContext, val system: ActorSystem[_]) {
 
