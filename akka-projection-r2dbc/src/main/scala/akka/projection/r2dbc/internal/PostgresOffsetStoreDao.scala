a6ae7908b9f79777d44d321362b8b8dfdae98478
Patrik Nordwall <patrik.nordwall@gmail.com>
2024-09-12 15:13:12 +0200

    perf: Try to load previous seqNr if missing, in validation
    * makes it more safe to evict from memory, but keep more in database
    * a step towards lazy loading of offsets,
      but full lazy loading would require many more changes due to the concurrency model used in the R2dbcOffsetStore
    * otherwise it will use timestampOf, which are be more costly for gRPC projections
    * it still falls back to timestampOf as last resort

--- akka-projection-r2dbc/src/main/scala/akka/projection/r2dbc/internal/PostgresOffsetStoreDao.scala
+++ akka-projection-r2dbc/src/main/scala/akka/projection/r2dbc/internal/PostgresOffsetStoreDao.scala
@@ -68,6 +68,13 @@ private[projection] class PostgresOffsetStoreDao(
     SELECT projection_key, slice, persistence_id, seq_nr, timestamp_offset
     FROM $timestampOffsetTable WHERE slice BETWEEN ? AND ? AND projection_name = ?"""
 
+  private val selectOneTimestampOffsetSql: String =
+    sql"""
+    SELECT seq_nr, timestamp_offset
+    FROM $timestampOffsetTable WHERE slice = ? AND projection_name = ? AND persistence_id = ?
+    ORDER BY seq_nr DESC
+    LIMIT 1"""
+
   private val insertTimestampOffsetSql: String =
     sql"""
     INSERT INTO $timestampOffsetTable
@@ -217,6 +224,23 @@ private[projection] class PostgresOffsetStoreDao(
         val seqNr = row.get("seq_nr", classOf[java.lang.Long])
         val timestamp = row.getTimestamp("timestamp_offset")
         R2dbcOffsetStore.RecordWithProjectionKey(R2dbcOffsetStore.Record(slice, pid, seqNr, timestamp), projectionKey)
+      })
+  }
+
+  def readTimestampOffset(slice: Int, pid: String): Future[Option[R2dbcOffsetStore.Record]] = {
+    r2dbcExecutor.selectOne("read one timestamp offset")(
+      conn => {
+        logger.trace("reading one timestamp offset for [{}] pid [{}]", projectionId, pid)
+        conn
+          .createStatement(selectOneTimestampOffsetSql)
+          .bind(0, slice)
+          .bind(1, projectionId.name)
+          .bind(2, pid)
+      },
+      row => {
+        val seqNr = row.get("seq_nr", classOf[java.lang.Long])
+        val timestamp = row.getTimestamp("timestamp_offset")
+        R2dbcOffsetStore.Record(slice, pid, seqNr, timestamp)
       })
   }
 
