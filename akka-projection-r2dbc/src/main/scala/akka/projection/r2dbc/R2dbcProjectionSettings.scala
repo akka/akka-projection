0558e3b462f3e9dc0abb9d72ab2384d5b0ef023e
Patrik Nordwall <patrik.nordwall@gmail.com>
2024-11-20 19:46:41 +0100

    detach delete from evict
    * delete much later, still based on latest by slice
    * delete-after config
    * increase delete-interval

--- akka-projection-r2dbc/src/main/scala/akka/projection/r2dbc/R2dbcProjectionSettings.scala
+++ akka-projection-r2dbc/src/main/scala/akka/projection/r2dbc/R2dbcProjectionSettings.scala
@@ -51,14 +51,29 @@ object R2dbcProjectionSettings {
       case _     => config.getDuration("offset-store.adopt-interval")
     }
 
+    val backtrackingWindow = config.getDuration("offset-store.backtracking-window")
+    val timeWindow = {
+      val d = config.getDuration("offset-store.time-window")
+      // can't be less then backtrackingWindow
+      if (d.compareTo(backtrackingWindow) < 0) backtrackingWindow
+      else d
+    }
+    val deleteAfter = {
+      val d = config.getDuration("offset-store.delete-after")
+      // can't be less then timeWindow
+      if (d.compareTo(timeWindow) < 0) timeWindow
+      else d
+    }
+
     new R2dbcProjectionSettings(
       schema = Option(config.getString("offset-store.schema")).filterNot(_.trim.isEmpty),
       offsetTable = config.getString("offset-store.offset-table"),
       timestampOffsetTable = config.getString("offset-store.timestamp-offset-table"),
       managementTable = config.getString("offset-store.management-table"),
       useConnectionFactory = config.getString("use-connection-factory"),
-      timeWindow = config.getDuration("offset-store.time-window"),
-      backtrackingWindow = config.getDuration("offset-store.backtracking-window"),
+      timeWindow,
+      backtrackingWindow,
+      deleteAfter,
       keepNumberOfEntries = 0,
       evictInterval = JDuration.ZERO,
       deleteInterval,
@@ -85,6 +100,7 @@ final class R2dbcProjectionSettings private (
     val useConnectionFactory: String,
     val timeWindow: JDuration,
     val backtrackingWindow: JDuration,
+    val deleteAfter: JDuration,
     @deprecated("Not used, evict is only based on time window", "1.6.3")
     val keepNumberOfEntries: Int,
     @deprecated("Not used, evict is not periodic", "1.6.2")
@@ -126,6 +142,12 @@ final class R2dbcProjectionSettings private (
 
   def withBacktrackingWindow(backtrackingWindow: JDuration): R2dbcProjectionSettings =
     copy(backtrackingWindow = backtrackingWindow)
+
+  def withDeleteAfter(deleteAfter: FiniteDuration): R2dbcProjectionSettings =
+    copy(deleteAfter = deleteAfter.toJava)
+
+  def withDeleteAfter(deleteAfter: JDuration): R2dbcProjectionSettings =
+    copy(deleteAfter = deleteAfter)
 
   @deprecated("Not used, evict is only based on time window", "1.6.2")
   def withKeepNumberOfEntries(keepNumberOfEntries: Int): R2dbcProjectionSettings =
@@ -175,6 +197,7 @@ final class R2dbcProjectionSettings private (
       useConnectionFactory: String = useConnectionFactory,
       timeWindow: JDuration = timeWindow,
       backtrackingWindow: JDuration = backtrackingWindow,
+      deleteAfter: JDuration = deleteAfter,
       deleteInterval: JDuration = deleteInterval,
       adoptInterval: JDuration = adoptInterval,
       logDbCallsExceeding: FiniteDuration = logDbCallsExceeding,
@@ -189,6 +212,7 @@ final class R2dbcProjectionSettings private (
       useConnectionFactory,
       timeWindow,
       backtrackingWindow,
+      deleteAfter,
       keepNumberOfEntries,
       evictInterval,
       deleteInterval,
