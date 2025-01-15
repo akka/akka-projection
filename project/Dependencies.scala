5fb32d01fd54c2403905848db80c0fd0c7be377f
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-01-15 09:52:48 +0100

    bump: akka-persistence-r2dbc 1.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -35,7 +35,7 @@ object Dependencies {
     val AkkaPersistenceCassandra = "1.3.0"
     val AkkaPersistenceJdbc = "5.5.0"
 
-    val AkkaPersistenceR2dbc = "1.3.0"
+    val AkkaPersistenceR2dbc = "1.3.1"
     val AkkaPersistenceR2dbcVersionInDocs = VersionNumber(AkkaPersistenceR2dbc).numbers match {
       case Seq(major, minor, _*) => s"$major.$minor"
     }
