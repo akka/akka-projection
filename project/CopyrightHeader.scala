0f2506555b8bf330a30fb9bd25d386b225b46e84
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-01-28 17:14:44 +0100

    chore: Update copyright year 2025 (#1308)

--- project/CopyrightHeader.scala
+++ project/CopyrightHeader.scala
@@ -20,7 +20,7 @@ object CopyrightHeader extends AutoPlugin {
 
   // We hard-code this so PR's created in year X will not suddenly fail in X+1.
   // Of course we should remember to update it early in the year.
-  val CurrentYear = "2024"
+  val CurrentYear = "2025"
   val CopyrightPattern = "Copyright \\([Cc]\\) (\\d{4}(-\\d{4})?) (Lightbend|Typesafe) Inc. <.*>".r
   val CopyrightHeaderPattern = s"(?s).*${CopyrightPattern}.*".r
 
